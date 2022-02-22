ac828800698e90d276c68621c7c74cd15d13b498
Taegyun Eom <eomtaegyun08@gmail.com>
2022-02-22 10:49:09 +0900

    config file

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const { Intents } = require("discord.js");
 
 const config = {
-    token: "OTQ1MTEzODEzOTY2MDA0Mjg0.YhLb1A.2IJsfracpdXdEpoktSxo9LTpVJw",
-    intent: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-    clientId: "945113813966004284",
-    guildId: "918796901342662667"
+    token: "",
+    intent: [],
+    clientId: "",
+    guildId: ""
 }
 
 module.exports = config;
